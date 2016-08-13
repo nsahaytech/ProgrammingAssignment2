2aa349099367c73a01de0078d68d3ee571316a83
nsahaytech <nsahaytech@gmail.com>
2016-08-13 19:27:08 +0530

    removing matrix check as its not required and updating functions documentation

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,6 @@
 ## Functions that cache the inverse of a matrix so that when we need it again, 
 ## it can be looked up in the cache rather than recomputed.
 
-## Usage example:
-##
-## > source('cachematrix.R')
-## > m <- makeCacheMatrix(matrix(c(2, 0, 0, 2), c(2, 2)))
-## > cacheSolve(m)
-## [,1] [,2]
-## [1,]  0.5  0.0
-## [2,]  0.0  0.5
-
 ## Create a special "matrix", which is a list containing
 ## a function to
 ##   - set the value of the matrix
@@ -33,14 +24,12 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 ## The following function calculates the inverse of the special "matrix" created 
-## with the above function. However, it first checks to see if the inverse has 
-## already been calculated. If so, it gets the inverse from the cache and skips 
-## the computation. Otherwise, it calculates the inverse of the data and sets 
-## the value of the inverse in the cache via the solve function.
+## with the above function. It first checks to see if the inverse has already 
+## been calculated. If so, it gets the inverse from the cache and skips the 
+## computation. Otherwise, it calculates the inverse of the data and sets the 
+## value of the inverse in the cache via the solve function.
 
 cacheSolve <- function(x, ...) {
-        if(!is.matrix(x)) stop("x must be a matrix")
-        # I want to make sure x is of type "matrix"
         ## Return a matrix that is the inverse of 'x'
         xinv <- x$getinv()
         if(!is.null(xinv)) {
